853b55e2aed376154cbd51de3f2ca27daa5280e6
Daniil <dan.games@inbox.ru>
2024-04-06 11:15:13 +0300

    DEMO

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import loginRouter from "./routes/login.js";
 import registrationRouter from "./routes/registration.js";
 import adminRouter from "./routes/admin.js";
 import userRouter from "./routes/user.js";
+import bronRouter from "./routes/bron.js"
 
 dotenv.config();
 const app = express();
@@ -30,6 +31,7 @@ app.use("/", loginRouter);
 app.use("/registration", registrationRouter);
 app.use("/admin", adminRouter);
 app.use("/user", userRouter);
+app.use("/bron", bronRouter);
 
 app.listen(PORT, () => {
 	console.log(`Сервер запущен на http://localhost:${PORT}`);
